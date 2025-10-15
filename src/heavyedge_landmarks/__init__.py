357f9bb163b90159cca3aae5243280819d86954f
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-10-15 10:07:48 +0900

    feat: landmarks_type3

--- src/heavyedge_landmarks/__init__.py
+++ src/heavyedge_landmarks/__init__.py
@@ -1,9 +1,10 @@
 """Package to locate landmarks from edge profiles."""
 
-from .mathematical import landmarks_type2
+from .mathematical import landmarks_type2, landmarks_type3
 from .pseudo import pseudo_landmarks
 
 __all__ = [
     "pseudo_landmarks",
     "landmarks_type2",
+    "landmarks_type3",
 ]
