cf8e6f2f2c285bcb6acef59d1b5f2b790a2ba0a3
JSS95 <52185322+JSS95@users.noreply.github.com>
2025-11-22 23:29:01 +0900

    Landmarks type1 (#8)

--- src/heavyedge_landmarks/mathematical.py
+++ src/heavyedge_landmarks/mathematical.py
@@ -5,9 +5,75 @@ from scipy.ndimage import gaussian_filter1d
 from scipy.signal import find_peaks, peak_prominences
 
 __all__ = [
+    "landmarks_type1",
     "landmarks_type2",
     "landmarks_type3",
 ]
+
+
+def landmarks_type1(x, Ys, Ls, sigma):
+    """Mathematical landmarks for type 1 heavy edge profiles.
+
+    Type 1 heavy edge profiles is a smooth profile with negligible or absent peak.
+    The following landmarks are detected:
+
+    1. Contact point.
+    2. Knee point between plateau and contact point.
+    3. Maximum point.
+
+    Parameters
+    ----------
+    x : array of shape (M,)
+        X grid of profiles.
+    Ys : array of shape (N, M)
+        Height data of N profiles.
+    Ls : array of shape (N,) and dtype=int
+        Length of each profile.
+    sigma : scalar
+        Standard deviation of Gaussian filter for smoothing.
+
+    Returns
+    -------
+    array of shape (N, 2, 3)
+        X and Y coordinates of landmarks.
+
+    Examples
+    --------
+    >>> from heavyedge import get_sample_path, ProfileData
+    >>> from heavyedge_landmarks import landmarks_type1
+    >>> with ProfileData(get_sample_path("Prep-Type1.h5")) as data:
+    ...     x = data.x()
+    ...     Ys, Ls, _ = data[:]
+    >>> lm = landmarks_type1(x, Ys, Ls, 32)
+    >>> lm.shape
+    (18, 2, 3)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... plt.plot(x, Ys.T, color="gray", alpha=0.5)
+    ... plt.plot(*lm.transpose(1, 2, 0))
+    """
+    ret = []
+    for Y, L in zip(Ys, Ls):
+        idxs = _landmarks_type1(Y[:L], sigma)
+        ret.append([x[idxs], Y[idxs]])
+    return np.array(ret)
+
+
+def _landmarks_type1(Y, sigma):
+    cp = len(Y) - 1
+
+    Y_smooth = gaussian_filter1d(Y, sigma)
+
+    pts = np.column_stack([np.arange(len(Y_smooth)), Y_smooth])
+    x, y = pts - pts[0], pts[-1] - pts[0]
+    dists = x[..., 0] * y[..., 1] - x[..., 1] * y[..., 0]
+    slope = np.diff(dists)
+    (extrema,) = np.nonzero(np.diff(np.sign(slope)))
+    K_neg = extrema[slope[extrema] < 0]
+    knee = K_neg[np.argmax(np.abs(dists[K_neg]))]
+
+    maximum = np.argmax(Y_smooth)
+
+    return np.array([cp, knee, maximum])
 
 
 def landmarks_type2(x, Ys, Ls, sigma):
